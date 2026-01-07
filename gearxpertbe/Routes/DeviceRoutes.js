6f4598d10147225257a078d0987721ab56dd12ec
thanhltde <149461786+Liamm-shkerpear@users.noreply.github.com>
2026-01-07 22:36:41 +0700

    feat(frontend): update homepage ui

--- gearxpertbe/Routes/DeviceRoutes.js
+++ gearxpertbe/Routes/DeviceRoutes.js
@@ -2,6 +2,7 @@ const routerDevice = require('express').Router();
 const deviceController = require('../controllers/Device/DeviceController');
 const { checkAccessToken } = require('../middleware/JWTAction');
 
+routerDevice.get('/', deviceController.getDevices);
 routerDevice.get('/:id',checkAccessToken, deviceController.getDeviceDetail);
 routerDevice.get('/:id/addons',checkAccessToken, deviceController.getDeviceAddons);
 routerDevice.get('/:id/related',checkAccessToken, deviceController.getRelatedDevices);
