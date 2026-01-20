6dda0d612f5b2cd072f85e98c0131b260a12ae74
cuongdeverloper <136320623+cuongdeverloper@users.noreply.github.com>
2026-01-20 13:09:28 +0700

    fix lack url

--- gearxpertbe/Routes/GoogleAuthRoutes.js
+++ gearxpertbe/Routes/GoogleAuthRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const { createRefreshToken, createJWT } = require('../middleware/JWTAction');
+const FRONTEND_URL = process.env.FRONTEND_URL
 
 router.get('/auth/google',
     passport.authenticate('google', { scope: ['profile', 'email'] }));
